872d8a76ea46d41a72aab3a3a6ae28473d2880cc
a8anassis <a8anassis@gmail.com>
2025-05-11 22:52:48 +0300

    fix: add printf with %4d

--- src/gr/aueb/cf/helloapp/SubApp.java
+++ src/gr/aueb/cf/helloapp/SubApp.java
@@ -4,6 +4,7 @@ public class SubApp {
 
     public static void main(String[] args) {
         System.out.println(sub(12, 2));
+        System.out.printf("%4d", sub(12, 2));
     }
 
     public static int sub(int a, int b) {
